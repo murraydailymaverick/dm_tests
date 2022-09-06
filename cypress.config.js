7c583401ef7a44139a3aa62bdc295b0cf0c2b4fc
Murray <info@leadfundi.com>
2022-09-06 14:45:21 +0200

    added some config for tests

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,54 @@ module.exports = defineConfig({
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
     },
+    env: {
+      baseUrl: 'https://dev.dailymaverick.co.za/',
+      dashboardUrl: 'https://dev.dailymaverick.co.za/wp-admin/',
+      localfolder: '',
+      domain: 'dev.dailymaverick.co.za',
+      users: {
+        admin: {
+          username: process.env.USERNAME+'_admin',
+          email: process.env.USERNAME+'+admin@dailymaverick.co.za',
+          pw: process.env.USER_PASSWORD
+        },
+        insider1: {
+          username: 'mr_insider1',
+          email: process.env.USERNAME+'+insider1@dailymaverick.co.za',
+          pw: process.env.USER_PASSWORD,
+          salutation: 'Mr',
+          firstname: process.env.USERNAME+'_Insider',
+          middlename: 'Dorris',
+          knownas: 'Bert',
+          first_language: 'English',
+          lastname: 'One',
+          tel: '0727322817',
+          residential_address: '34 Eagles Crag',
+          residential_suburb: 'Lone Hill',
+          residential_city: 'Johannesburg',
+          residential_province: 'Gauteng',
+          residential_country: 'South Africa',
+          residential_code: '2191',
+          occupation: 'Software Engineer'
+        },
+        insider2: {
+          username: 'mr_insider2',
+          email: process.env.USERNAME+'+insider2@dailymaverick.co.za',
+          pw: process.env.USER_PASSWORD,
+          salutation: 'Mr',
+          firstname: process.env.USERNAME+'_Insider',
+          lastname: 'Two'
+        },
+        insider3: {
+          username: 'mr_insider3',
+          email: process.env.USERNAME+'+insider3@dailymaverick.co.za',
+          pw: process.env.USER_PASSWORD,
+          salutation: 'Mr',
+          firstname: process.env.USERNAME+'_Insider',
+          lastname: 'Three'
+        }
+      }
+    },
   },
 })
 
