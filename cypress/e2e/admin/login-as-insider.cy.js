fe84e2dde6a5304b599f0b4f69526e9279a18a56
Murray <info@leadfundi.com>
2022-09-08 10:19:42 +0200

    local login-as-insider is working

--- cypress/e2e/admin/login-as-insider.cy.js
+++ cypress/e2e/admin/login-as-insider.cy.js
@@ -7,36 +7,23 @@ context( 'Login as an admin, create a new insider via subscriptions' , function 
 		cy.clearWordPressCookies();
 	});
 
-	// beforeEach(function () {
-	// 	cy.clearWordPressCookies();
-	// });
-
-	// it( 'logs out the user - and logs back in using setting wp-cookies', function(){
-	// 	cy.visit( Cypress.env('dashboardUrl') );
-	// 	cy.location('pathname').should( 'eq', Cypress.env('localfolder')+'/wp-admin/' ); // Is logged in
-	// });
-
-	it( 'Logs in a admin user', function(){
-		cy.manualWordPressAdminLogin();
-		cy.getWordPressCookies();
-		cy.visit( Cypress.env('dashboardUrl') );
-		cy.location('pathname').should( 'eq', Cypress.env('localfolder')+'/wp-admin/' );
+	beforeEach(function () {
+		cy.setWordPressCookies();
 	});
 
 
 	it( 'As an Admin - Get a token, Logout.', function(){
 
-		cy.setWordPressCookies();
-		//http://localhost/dm/wp-admin/admin.php?page=mailloginengine_encode
 		cy.visit( Cypress.env('dashboardUrl') +  'admin.php?page=mailloginengine_encode');
 
-		cy.get('span#current_user_id').then(($span) => {
-			cy.wrap($span.text()).as('current_user_id');
-		});
-
+		// cy.get('span#current_user_id').then(($span) => {
+		// 	cy.wrap($span.text()).as('current_user_id');
+		// });
+		//
 		// cy.get('@current_user_id').then(current_user_id => {
 		// 	cy.get('input[name=user_id]').clear().type(current_user_id);
 		// });
+		cy.get('input[name=user_id]').clear().type(insider1.id);
 
 		cy.get('input#submit').click();
 
@@ -52,18 +39,15 @@ context( 'Login as an admin, create a new insider via subscriptions' , function 
 
 	});
 
-//then test ur to see if logged in
-	it( 'Logs in with a token', function(){
+
+	it( 'Logs in with a token and checks that you are logged in', function(){
 
 		cy.task('getToken').then((token) => {
-			cy.visit( Cypress.env('baseUrl') +  '?token=' + token);
+			//cy.visit( Cypress.env('baseUrl') +  '?token=' + token + '&maillogindebug=1');
+			cy.visit( Cypress.env('baseUrl') +  '?token=' + token );
 		});
 
 		cy.get('button.navbar-toggle').click()
+		cy.get('li.login-mobile-profile a').should('have.length', 6)
 	});
-
-	it( 'deletes a user', function(){
-		cy.deleteUser(insider1);
-	});
-
 });
