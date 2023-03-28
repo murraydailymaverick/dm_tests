1c83c95731f258d2263346a8d5461e015572340a
murraygreig <murray@dailymaverick.co.za>
2023-03-28 10:47:21 +0200

    moved elemetor test to percy

--- cypress/e2e/percy/elementor-pages.cy.js
+++ cypress/e2e/percy/elementor-pages.cy.js
@@ -7,6 +7,7 @@ context( 'Lets test if the elementor pages look consistent' , function () {
         cy.visit( Cypress.env('baseUrl') + '/article/2018-06-04-distinctive-sophistication/');
         cy.get('div.titles h4').should('have.text', 'SPONSORED CONTENT')
         //cy.get('div.titles h1').should('have.text', 'Distinctive Sophistication: A Tsogo Sun Experience')
+        cy.percySnapshot();
     });
 
     it( 'visits a questions page', function(){
@@ -14,6 +15,7 @@ context( 'Lets test if the elementor pages look consistent' , function () {
         cy.get('.elementor-heading-title.elementor-size-xl').should('have.text', '22 Questions with Alet Law')
         cy.get('div.elementor-toggle-item').should('have.length', 22)
         cy.get('.elementor-toggle-icon .elementor-toggle-icon-closed .e-font-icon-svg').should('have.length', 22);
+        cy.percySnapshot();
     });
 
 });
