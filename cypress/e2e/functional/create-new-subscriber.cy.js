a24d2f8db1f9d44e53d312f01d67807c90e95049
murraygreig <murray@dailymaverick.co.za>
2023-03-28 15:54:29 +0200

    fixed subscriber tests

--- cypress/e2e/functional/create-new-subscriber.cy.js
+++ cypress/e2e/functional/create-new-subscriber.cy.js
@@ -8,49 +8,6 @@ context( 'Create a new user via the API and ads a user meta' , function () {
     });
 
 
-    // it( 'registers via the from the wp /sign-in/ form.', function(){
-    //     cy.intercept('POST', Cypress.env('dashboardUrl') + '/admin-ajax.php').as('ajaxPost');
-    //
-    //     cy.visit( Cypress.env('baseUrl') +'?utm_source=testing&utm_medium=testing&utm_campaign=testing&utm_term=testing&utm_content=testing');
-    //     cy.visit( Cypress.env('loginUrl'));
-
-    //     cy.get('ul.nav-tabs li a[href="#register"]').click();
-    //     cy.get('input#email').type(subscriber.email);
-    //     cy.get('input#password').type(subscriber.pw);
-    //     cy.get('input#agree_terms').click();
-    //     cy.get('input[name="user_registration"]').click();
-    //     cy.wait('@ajaxPost');
-    //     cy.wait(2000);
-    //
-    //     cy.location('pathname').should( 'contain', Cypress.env('localfolder')+'/sign-in' );
-    //
-    //
-    //     cy.get('span.heading-step').should('be.visible' );
-    //     cy.get('span.heading-step:first').should( "have.text", "Step 2 of 3" );
-    //     // lets subscribe to a newsletter? or check the count?
-    //     cy.get('div.newsletter-container').children().should('have.length', 12);
-    //     cy.get('input#user-segment0').should('have.class', 'plus-minus');
-    //
-    //     cy.get('input#user-segment0').should('be.checked' );
-    //     cy.get('input#user-segment0').click()
-    //     cy.get('input#user-segment0').should('not.be.checked' );
-    //
-    //     cy.get('input.subscribe-btn').scrollIntoView().click();
-    //     cy.wait('@ajaxPost');
-    //     cy.get('.toast-message')
-    //         .should('be.visible' )
-    //         //.should('have.text', 'Newsletter Preferences updated' );
-    //
-    //     //cy.get('span.heading-step').should( "have.text", "Step 3 of 3" );
-    //     cy.get('a.btn-full').should('have.attr', 'href').and('include', '/insider/').then((href) => {
-    //         cy.visit( Cypress.env('baseUrl') + href)
-    //         cy.location('pathname').should( 'contain', '/insider/' );
-    //         //cy.getWordPressCookies('subscriber');
-    //     })
-    // });
-
-    //it( 'registers via the from the reg-wall.', function(){});
-
     it( 'registers via the the navigation email doesnt exist.', function(){
         cy.viewport(1440, 1024);
         cy.intercept('POST', Cypress.env('dashboardUrl') + '/admin-ajax.php').as('ajaxPost');
@@ -75,22 +32,68 @@ context( 'Create a new user via the API and ads a user meta' , function () {
         //     cy.get('.editor-toolbar-actions-save').click();
         //     cy.url().should('not.eq', url);
         // });
-       // var useridValue = 0
+        // var useridValue = 0
         //cy.get('head meta[name=user_id]').invoke('attr', 'content').then(  (userid) => {});
-       // cy.log('The attribute values is:' + useridValue)
-        cy.wait(5000);
-        cy.request('POST', Cypress.env('baseUrl') +'wp-json/dm_rest_api/v1/users/delete', '{ "email": "'+subscriber.email+'" }');
+        // cy.log('The attribute values is:' + useridValue)
     });
 
-  /*  it( 'logs in as a reader', function(){
+    it( 'deletes a User', function(){
+        cy.deleteUser(subscriber);
+    });
+
+    it( 'registers via the from the wp /sign-in/ form.', function(){
+        cy.intercept('POST', Cypress.env('dashboardUrl') + '/admin-ajax.php').as('ajaxPost');
+
+        cy.visit( Cypress.env('baseUrl') +'?utm_source=testing&utm_medium=testing&utm_campaign=testing&utm_term=testing&utm_content=testing');
+        cy.visit( Cypress.env('loginUrl'));
+
+        cy.get('ul.nav-tabs li a[href="#register"]').click();
+        cy.get('input#email').type(subscriber.email);
+        cy.get('input#password').type(subscriber.pw);
+        cy.get('input#agree_terms').click();
+        cy.get('input[name="user_registration"]').click();
+        cy.wait('@ajaxPost');
+        cy.wait(2000);
+
+        cy.location('pathname').should( 'contain', Cypress.env('localfolder')+'/sign-in' );
+
+
+        cy.get('span.heading-step').should('be.visible' );
+        cy.get('span.heading-step:first').should( "have.text", "Step 2 of 3" );
+        // lets subscribe to a newsletter? or check the count?
+        cy.get('div.newsletter-container').children().should('have.length', 12);
+        cy.get('input#user-segment0').should('have.class', 'plus-minus');
+
+        cy.get('input#user-segment0').should('be.checked' );
+        cy.get('input#user-segment0').click()
+        cy.get('input#user-segment0').should('not.be.checked' );
+
+        cy.get('input.subscribe-btn').scrollIntoView().click();
+        cy.wait('@ajaxPost');
+        cy.get('.toast-message')
+            .should('be.visible' )
+            //.should('have.text', 'Newsletter Preferences updated' );
+
+        //cy.get('span.heading-step').should( "have.text", "Step 3 of 3" );
+        cy.get('a.btn-full').should('have.attr', 'href').and('include', '/insider/').then((href) => {
+            cy.visit( Cypress.env('baseUrl') + href)
+            cy.location('pathname').should( 'contain', '/insider/' );
+            cy.getWordPressCookies('subscriber');
+        })
+    });
+
+    //it( 'registers via the from the reg-wall.', function(){});
+
+    it( 'logs in as a reader', function(){
         cy.intercept('POST', '/ossc-api/create-order/').as('ajaxCreateOrder');
         cy.intercept('POST', '/ossc-api/generate-payment-gateway-signature/').as('ajaxCreateSignature');
-        cy.intercept('POST', 'https://sandbox.payfast.co.za/eng/method/WalletFunds/!*').as('ajaxWalletFunds');
-        cy.setWordPressCookies('reader');
+        cy.intercept('POST', 'https://sandbox.payfast.co.za/eng/method/WalletFunds/*').as('ajaxWalletFunds');
+        cy.setWordPressCookies('subscriber');
         cy.visit(Cypress.env('baseUrl'));
-        cy.get('button.navbar-toggle').click()
-        cy.get('li.login-mobile-profile a').should('have.length', 6)
-        cy.get('li.login-mobile-profile a.profile-link').should('contain.text', 'mr_subscriber')
+        cy.get('.footer').scrollIntoView();
+        //cy.get('button.navbar-toggle.navbar-toggle-right').click()
+        // cy.get('li.login-mobile-profile a').should('have.length', 6)
+        // cy.get('li.login-mobile-profile a.profile-link').should('contain.text', 'mr_subscriber')
         cy.visit(Cypress.env('baseUrl')+'/manage-membership/');
 
         cy.visit(Cypress.env('insiderUrl'));
@@ -117,9 +120,7 @@ context( 'Create a new user via the API and ads a user meta' , function () {
 
     });
 
- */
     //check user
-/*
     it( 'checks the new users data', function(){
         cy.setWordPressCookies('admin');
         cy.visit(Cypress.env('dashboardUrl') + 'users.php');
@@ -176,13 +177,10 @@ context( 'Create a new user via the API and ads a user meta' , function () {
 
 
     });
-*/
 
-/*
    it( 'deletes a User', function(){
         cy.deleteUser(subscriber);
    });
-*/
 
 });
 Cypress.on('uncaught:exception', (err, runnable) => {
