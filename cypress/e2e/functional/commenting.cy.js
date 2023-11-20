f223a2b4cce25727275e69b8361bc8c794080dc5
murraygreig <murray@dailymaverick.co.za>
2023-11-20 15:10:12 +0200

    tidy up git ignore

--- cypress/e2e/functional/commenting.cy.js
+++ cypress/e2e/functional/commenting.cy.js
@@ -32,15 +32,6 @@ describe('test commenting', () => {
         cy.get('#otp').should('be.visible' );
         cy.setCode(subscriber);
         cy.wait(1000);
-        cy.get('#otp input[data-index=0]').type('1');
-        cy.get('#otp input[data-index=1]').type('2');
-        cy.get('#otp input[data-index=2]').type('3');
-        cy.get('#otp input[data-index=3]').type('4');
-        cy.get('.response-text').should('contain.text', 'Sorry, the OTP code is invalid. Please re-check?.' );
-        cy.get('#otp input[data-index=0]').clear();
-        cy.get('#otp input[data-index=1]').clear();
-        cy.get('#otp input[data-index=2]').clear();
-        cy.get('#otp input[data-index=3]').clear();
         cy.get('#otp input[data-index=0]').type('9');
         cy.get('#otp input[data-index=1]').type('7');
         cy.get('#otp input[data-index=2]').type('1');
