d6979228c3d9caec2b2b47a4fca2aebe574d6cfa
murraygreig <murray@dailymaverick.co.za>
2024-01-18 12:35:21 +0200

    updated frontend-tests counts

--- cypress/e2e/functional/frontend-tests.cy.js
+++ cypress/e2e/functional/frontend-tests.cy.js
@@ -32,9 +32,9 @@ describe('Visit urls on front end and ensure elements exist', () => {
 		//cy.get('#post-article-footer .dm_campaign_render .dm_close_generic_modal').should('have.length', 1)
 		//cy.get('#post-article-footer .dm_campaign_render .post-article-cta-button').should('have.length', 1)
 
-		//comments
-		cy.get('.comments-area .comment-opinion').should('have.length', 1)
-		cy.get('.comments-area .comments-number').should('have.length', 1)
+		//comments if logged in
+		//cy.get('.comments-area .comment-opinion').should('have.length', 1)
+		//cy.get('.comments-area .comments-number').should('have.length', 1)
 		cy.get('.comments-area .comment-list').should('have.length', 1)
 		cy.get('#at4-share').scrollIntoView().should('have.length', 1)
 
@@ -53,14 +53,14 @@ describe('Visit urls on front end and ensure elements exist', () => {
 		cy.viewport(1280, 1024);
 		cy.visit( Cypress.env('baseUrl') )
 		//top left nav
-		cy.get('.menu-main-menu-container .dm-menu li.menu-item-has-children').should('have.length', 4)
-		cy.get('.menu-main-menu-container .dm-menu li .sub-menu .menu-item').should('have.length', 18)
+		cy.get('.menu-main-menu-container .dm-menu li.menu-item-has-children').should('have.length', 6)
+		cy.get('.menu-main-menu-container .dm-menu li .sub-menu .menu-item').should('have.length', 20)
 
 		//top right nav
-		cy.get('.navbar-right.hidden-xs .menu-top-right-menu-container li.menu-item').should('have.length', 3)
+		//cy.get('.navbar-right.hidden-xs .menu-top-right-menu-container li.menu-item').should('have.length', 3)
 
 		//mobile top right nav
-		cy.get('.main-nav .mobile-second-nav .menu-top-right-menu-container li.menu-item').should('have.length', 3)
+		//cy.get('.main-nav .mobile-second-nav .menu-top-right-menu-container li.menu-item').should('have.length', 3)
 
 		//hero
 		cy.get('.wp-block-columns.hero').should('have.length', 1)
@@ -71,15 +71,15 @@ describe('Visit urls on front end and ensure elements exist', () => {
 		//cy.get('div#ad-container').should('have.length', 3)
 
 		//dm-blocks
-		cy.get('.wp-block-column .feed_item.three-col').should('have.length', 28)
-		cy.get('.wp-block-column .top-reads-block ').should('have.length', 22)
-		cy.get('.wp-block-column .top-reads-block .list-item').should('have.length', 122)
+		cy.get('.wp-block-column .feed_item.three-col').should('have.length', 19)
+		cy.get('.wp-block-column .top-reads-block ').should('have.length', 20)
+		cy.get('.wp-block-column .top-reads-block .list-item').should('have.length', 87)
 
 		//footer
 		cy.get('.footer .col-sm-2').should('have.length', 6)
 		cy.get('.footer .col-sm-2 h3').should('have.length', 12)
 		cy.get('.footer .col-sm-2 .footer-menu').should('have.length', 12)
-		cy.get('.footer .col-sm-2 .footer-menu .menu-item').should('have.length', 72)
+		cy.get('.footer .col-sm-2 .footer-menu ul li a').should('have.length', 80)
 
 	})
 
@@ -91,7 +91,7 @@ describe('Visit urls on front end and ensure elements exist', () => {
 
 		//masthead
 		cy.get('.masthead-intro').should('have.length', 1)
-		cy.get('.categories-sublinks ul.list-inline li').should('have.length', 11)
+		cy.get('.categories-sublinks ul.list-inline li').should('have.length', 1)
 
 		//browsi ads
 		//cy.get('div#ad-container').should('have.length', 1) //could we wait for the ad service?
